03022f4e51d9740b53a729d04b0cf43f9862984a
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2025-08-04 19:35:15 +0300

    Initial commit

---
+++ src/test/java/dev/sertan/android/backend/AndroidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sertan.android.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
